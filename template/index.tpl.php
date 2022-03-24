8d95fbdf93f3ba90e7bb9c9bd073b514ac1c95f7
El houssaïne El jadid <ikky57@gmail.com>
2022-03-24 15:42:02 +0100

    new modification

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -2,19 +2,19 @@
 <div class="container">
     <h1>Mes QCMs</h1>
 
-    <table border="1">
+    <table class="table table-hover table-bordered border-dark">
         <thead>
-            <tr>
-                <th>Id</th>
-                <th>Titre</th>
-                <th>Actions</th>
+            <tr class="table-warning table-bordered border-dark">
+                <th scope="col">Id</th>
+                <th scope="col">Titre</th>
+                <th scope="col">Actions</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach($qcms as $qcm): ?>
             <tr>
-                <td><?= $qcm->getId() ?></td>
-                <td><?= $qcm->getTitle() ?></td>
+                <td class="table-danger"><?= $qcm->getId() ?></td>
+                <td class="table-info"><?= $qcm->getTitle() ?></td>
                 <td>
                     <a href="">Modifier</a>
                     <a href="">Supprimer</a>
